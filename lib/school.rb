98467f807dfd5d8fcdfcecddfae2d10a38a04853
linkcoding <kenny.lin.91@gmail.com>
2017-07-19 10:49:27 -0400

    created school class with grade, students, and roster

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] ? self.roster[grade] << name : self.roster[grade] = [name]
+  end
+
+  def grade(num)
+    self.roster[num]
+  end
+
+  def sort
+    self.roster.each do |grade, name|
+      self.roster[grade] = self.roster[grade].sort
+    end
+  end
+end
